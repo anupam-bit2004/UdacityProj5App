3b7001eac3862d61aa16b3faab9f25ebb3ca3023
anupam-bit2004 <anupam_bit2004@hotmail.com>
2020-11-28 22:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 import newspaper
 bbc_paper = newspaper.build('https://www.bbc.com/news', memoize_articles=False)
 first_article = bbc_paper.articles[1]
@@ -6,3 +7,14 @@ first_article.parse()
 print(first_article.title)
 first_article.nlp()
 print(first_article.summary)
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
